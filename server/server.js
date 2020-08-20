d33fc70a07ba60d82b6375f8e176bb36864217ef
Allan <allanl_95@hotmail.com>
2020-08-20 16:27:52 -0700

    version 1.0

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const passport = require('passport');
 const session = require('express-session');
+const path = require('path');
 
 // Private ENV
 dotenv.config({ path: './config/config.env' });
@@ -32,6 +33,15 @@ app.use(passport.session());
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
 app.use('/guests', require('./routes/guests'));
+
+// Serve static assests if in production
+if(process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('../client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
 
 const PORT = process.env.PORT || 5000;
 
