33f0280bf7f4f616f5d09a2f9cb2eb60814a2ee3
Allan <allanl_95@hotmail.com>
2020-08-20 17:00:06 -0700

    build frontend

---
+++ server/build/precache-manifest.cc1be1bcc47be499b671d9a81e599485.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a0aeba5ad7bd37d6d351098a537871",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a7324af91b88cc4321d",
+    "url": "/static/css/main.51e45eb9.chunk.css"
+  },
+  {
+    "revision": "d4682a3ec8f8c55f6c8d",
+    "url": "/static/js/2.7b710e42.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7b710e42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7324af91b88cc4321d",
+    "url": "/static/js/main.6c4420ff.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
